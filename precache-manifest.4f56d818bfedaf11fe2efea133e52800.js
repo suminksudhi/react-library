2d5c4c3a31ff924d0943f9375ebd17508a9dcfd2
suminksudhi <dev.sumin@yahoo.com>
2020-04-21 13:16:35 +0400

    Updates

---
+++ precache-manifest.4f56d818bfedaf11fe2efea133e52800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fca88d4e7fd3d698b8898e67f51c07",
+    "url": "/react-library-js/index.html"
+  },
+  {
+    "revision": "3eb471930ed6f6cf65f1",
+    "url": "/react-library-js/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "2e2dd17fea3c8ce349db",
+    "url": "/react-library-js/static/js/2.aa709366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-library-js/static/js/2.aa709366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb471930ed6f6cf65f1",
+    "url": "/react-library-js/static/js/main.6c7e0ec8.chunk.js"
+  },
+  {
+    "revision": "350676797bc31cf41e0c",
+    "url": "/react-library-js/static/js/runtime-main.acff16f3.js"
+  }
+]);
